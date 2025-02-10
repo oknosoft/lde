49705244b58e43c7f8747225ba2348c15fd219c5
unpete <info@oknosoft.ru>
2025-02-10 22:39:31 +0500

    example

--- example/node-test.js
+++ example/node-test.js
@@ -2,5 +2,9 @@
 const MetaEngine = require('../core/dist');
 // MetaEngine.plugin(...)
 const $p = global.$p = new MetaEngine();
+// параметры сеанса инициализируем сразу
+$p.jobPrm.init(require('./app.settings'));
+$p.md.init(require('./meta.json'));
+$p.md.createManagers([]);
 
-console.log($p.version);
+console.log($p.cat);
